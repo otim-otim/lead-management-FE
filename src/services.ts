cb311d31cf2373c7328aa8f49492021167dc8b10
Otim Isaac <isaacomega16@gmail.com>
2024-11-15 13:37:08 +0300

    add fetch users api call

--- src/services.ts
+++ src/services.ts
@@ -1,13 +1,14 @@
 import { axiosAdapter } from "./axiosAdapter";
 import { createFollowUpParams, FollowUpStatusEnum } from "./types";
+import { Lead, CreateLeadParams } from "./types";
 
-export async function fetchLeads() {
+export async function fetchLeads(): Promise<{ leads: Lead[] } | { error: any }> {
     try {
         const {data, status} = await axiosAdapter.get('/api/leads');
         if(status !== 200) {
             throw new Error('Failed to fetch leads');
         }
-        return { leads: data}
+        return { leads: data.data}
     } catch (error) {
         return { error }
     }
@@ -19,13 +20,13 @@ export async function fetchFollowups() {
         if(status !== 200) {
             throw new Error('Failed to fetch followups');
         }
-        return { followups: data}
+        return { followups: data.data}
     } catch (error) {
         return { error }
     }
 }
 
-export async function createFollowUp(params: any) {
+export async function createFollowUp(params: createFollowUpParams) {
     try {
         const {data, status} = await axiosAdapter.post('/api/followups', params);
         if(status !== 200) {
@@ -37,7 +38,7 @@ export async function createFollowUp(params: any) {
     }
 }
 
-export async function createLead(params: createFollowUpParams) {
+export async function createLead(params: CreateLeadParams) {
     try {
         const {data, status} = await axiosAdapter.post('/api/leads', params);
         if(status !== 200) {
@@ -56,6 +57,18 @@ export async function updateFollowUpStatus( id: number, newStatus: FollowUpStatu
             throw new Error('Failed to update followup');
         }
         return { followup: data}
+    } catch (error) {
+        return { error }
+    }
+}
+
+export async function fetchUsers() {
+    try {
+        const {data, status} = await axiosAdapter.get('/api/users');
+        if(status !== 200) {
+            throw new Error('Failed to fetch users');
+        }
+        return { users: data.data}
     } catch (error) {
         return { error }
     }
