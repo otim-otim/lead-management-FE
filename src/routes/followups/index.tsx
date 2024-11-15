550a5394c3f3d0493d758817b48412fa369fe558
Otim Isaac <isaacomega16@gmail.com>
2024-11-15 15:05:30 +0300

    restrict access to button for unauthorized personel

--- src/routes/followups/index.tsx
+++ src/routes/followups/index.tsx
@@ -2,13 +2,24 @@ import { useEffect, useState, FormEvent, ChangeEvent } from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 import { useNavigate } from '@tanstack/react-router'
 import { fetchFollowups } from '../../services'
-import { FollowUp } from '../../types'
+import  { FollowUp, FollowUpStatusEnum  } from '../../types'
 import ListGroup from 'react-bootstrap/ListGroup';
-import { Button } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { Button } from 'react-bootstrap'
+import { updateFollowUpStatus } from '../../services'
 
 export const Route = createFileRoute('/followups/')({
   component: RouteComponent,
 })
+
+const  enum FollowUpStatusEnum {
+  PENDING = 'Pending',
+  COMPLETED = 'Completed',
+  MISSED = 'Missed'
+}
+
+const currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
 
 function RouteComponent() {
   const [ followUps, setFollowUps ] = useState<FollowUp[]>([])
@@ -27,9 +38,24 @@ function RouteComponent() {
     fetchAndSetFollowUps();
   }, []);
 
+  function UpdateFollowUpStatusButton({ followup }: { followup: FollowUp }) {
+    
+    if(followup.status == FollowUpStatusEnum.MISSED || currentUser.role === 'sales_rep'){
+      return ''
+      
+    }
+
+    const newStatus = followup.status === FollowUpStatusEnum.PENDING ? { status: FollowUpStatusEnum.COMPLETED, action: 'Complete '} : {status: FollowUpStatusEnum.PENDING, action: 'Pend '} 
+    return  <Button onClick={() => updateFollowUpStatus(followup.id, newStatus.status)} >
+      {newStatus.action}follow up 
+    </Button>
+    
+  }
+
   return (
     <>
       <h1>Follow Ups</h1>
+     
       <ListGroup>
       {followUps.map((follow : FollowUp) => (
         <ListGroup.Item key={follow.id}>
@@ -40,7 +66,7 @@ function RouteComponent() {
             <p>
             <span>Sales Rep: {follow.user.name}</span>
             </p>
-          {/* <Button variant="primary" onClick={() => handleScheduleFollowUp(lead)}>schedule Follow up</Button> */}
+          {UpdateFollowUpStatusButton({ followup: follow })}
         </ListGroup.Item>
       ))}
       </ListGroup>
